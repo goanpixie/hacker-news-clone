0c00651bc2d91adbb97727cbcf98202c938b142e
Priyanka <priyanka3007@gmail.com>
2019-10-01 17:33:01 -0700

    making api request to hacker news server

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import GlobalStyles from 'styles/globals';
 import { Provider } from 'react-redux';
 import configureStore from 'store';
 import * as serviceWorker from './serviceWorker';
+import hackerNewsApi from 'services/hackerNewsApi';
+
+
+hackerNewsApi.getTopStoryIds().then(ids => console.log(ids));
 
 const renderApp = () => {
 	const initialState = {};
