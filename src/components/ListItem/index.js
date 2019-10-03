e83516ef8a5ed99e8073c1910d1255ecfe97bf89
Priyanka <priyanka3007@gmail.com>
2019-10-03 13:33:54 -0700

    added live content

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import Timeago from 'timeago.js';
+import getSiteHostname from 'utils/getSiteHostname';
+import getArticleLink, { HN_USER, HN_ITEM } from 'utils/getArticleLink';
+
 
 import { Item, Title, Host, ExternalLink, Description, CommentLink } from './styles';
 
 const LINK_REL = "nofollow noreferrer noopener";
+// const timeago = Timeago();
 
-const ListItem = () => {
+const ListItem = ({ kids=[], by , id, url, score, time, title, type }) => {
+  const site = getSiteHostname(url) || 'new.ycombinator.com';
+  const link = getArticleLink({ url, id });
+  const commentUrl = `${HN_ITEM}${id}`;
+  const userUrl = `${HN_USER}${by}`;
+
   return (
     <Item>
-      <ExternalLink >
+      <ExternalLink href={link} rel={LINK_REL} target="_blank">
         <Title>
-          Developer Community<Host>(gitconnected.com)</Host>
+          {title}<Host>({site})</Host>
         </Title>
       </ExternalLink>
       <Description>
-        9000 points by{' '}
-        <CommentLink href="#" rel={LINK_REL} target="_blank">
-          Test User
+        {score} points by{' '}
+        <CommentLink href={userUrl} rel={LINK_REL} target="_blank">
+          {by}
+          {' | '}
 				</CommentLink>
-				1 Hour Ago
+				1 hr ago
 				{' | '}
-        <CommentLink href="#" rel={LINK_REL} target="_blank">
-          42 Comments
+        <CommentLink href={commentUrl} rel={LINK_REL} target="_blank">
+          {kids.length} Comments
         </CommentLink>
       </Description>
     </Item>
