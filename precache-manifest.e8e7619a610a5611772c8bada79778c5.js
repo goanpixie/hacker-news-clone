b4eddd60c931725325cd1fe929b007585880be4a
Priyanka <priyanka3007@gmail.com>
2019-10-04 11:50:41 -0700

    Updates

---
+++ precache-manifest.e8e7619a610a5611772c8bada79778c5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69493d5e300c699f8f538bd9fcd83ebf",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "073b0136f3135955eaf4",
+    "url": "/hacker-news-clone/static/js/2.9677a91a.chunk.js"
+  },
+  {
+    "revision": "89f8a4acab2ada554553",
+    "url": "/hacker-news-clone/static/js/main.188865be.chunk.js"
+  },
+  {
+    "revision": "8e898cee19a93a1cb84f",
+    "url": "/hacker-news-clone/static/js/runtime-main.fc3786af.js"
+  }
+]);
